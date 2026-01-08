56eda607a1dc808316f7fbb032b164b04ec8265d
Mictronics <github@mictronics.de>
2026-01-08 21:12:47 +0100

    Removed obsolete variables.

--- packages/web/src/views/Dashboard.vue
+++ packages/web/src/views/Dashboard.vue
@@ -2,7 +2,7 @@
   <Toast position="bottom-right" />
   <main class="dashboard-view">
     <div class="w-full flex">
-      <Sidebar :node-count="nodeCount" :new-message-count="newMessageCount" :is-side-bar-visible="useAppStore().appData.isSideBarVisible" />
+      <Sidebar :is-side-bar-visible="useAppStore().appData.isSideBarVisible" />
       <div class="w-full h-full">
         <Header @event-toggle-sidebar="onToggleSidebar" />
         <div class="w-full h-[calc(100vh-50px)]">
@@ -18,9 +18,6 @@ import { ref } from 'vue';
 import { useAppStore } from '@/composables/core/stores/app/useAppStore';
 import Header from '@/components/Dashboard/Header.vue';
 import Sidebar from '@/components/Dashboard/Sidebar.vue';
-
-const nodeCount = ref(123);
-const newMessageCount = ref(5);
 
 function onToggleSidebar() {
   useAppStore().appData.isSideBarVisible = !useAppStore().appData.isSideBarVisible;
