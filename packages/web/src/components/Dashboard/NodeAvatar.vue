03f3d9250735bc5774f3e3295b0724704c6be714
Mictronics <github@mictronics.de>
2026-01-14 16:43:24 +0100

    Reduce avatar size.

--- packages/web/src/components/Dashboard/NodeAvatar.vue
+++ packages/web/src/components/Dashboard/NodeAvatar.vue
@@ -47,7 +47,7 @@ const avatarColor = computed(() => {
 /* 3D avatar */
 .node-avatar {
   position: relative;
-  padding: 3ch;
+  padding: 2.5ch;
   background: rgb(var(--bg-r), var(--bg-g), var(--bg-b));
   border-radius: 50%;
 
