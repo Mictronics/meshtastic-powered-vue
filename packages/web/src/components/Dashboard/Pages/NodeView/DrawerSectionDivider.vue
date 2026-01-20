da8f10841aa0570e73551dee46387d1918041bd2
Mictronics <github@mictronics.de>
2026-01-20 19:19:52 +0100

    Moved divider styles into component.

--- packages/web/src/components/Dashboard/Pages/NodeView/DrawerSectionDivider.vue
+++ packages/web/src/components/Dashboard/Pages/NodeView/DrawerSectionDivider.vue
@@ -7,3 +7,31 @@
 <script setup lang="ts">
 defineProps<{ title: string }>();
 </script>
+
+<style lang="css" scoped>
+.divider {
+  display: flex;
+  align-items: center;
+  text-align: center;
+}
+
+.divider::before,
+.divider::after {
+  content: '';
+  flex: 1;
+  border-bottom: 1px solid oklch(96.8% 0.007 247.896);
+}
+
+.dark .divider::before,
+.dark .divider::after {
+  border-bottom: 1px solid oklch(44.6% 0.043 257.281);
+}
+
+.divider::before {
+  margin-right: 10px;
+}
+
+.divider::after {
+  margin-left: 10px;
+}
+</style>
