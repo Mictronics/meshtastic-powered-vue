4c5932b52fad55eb3737ec5fe2c4f19dd7c53946
Mictronics <github@mictronics.de>
2026-01-23 20:36:57 +0100

    Added unread count to broadcast channels.

--- packages/web/src/components/Dashboard/Sidebar.vue
+++ packages/web/src/components/Dashboard/Sidebar.vue
@@ -299,21 +299,27 @@ const isChatView = computed(() => route.matched.some((r) => r.meta?.viewChat));
 const channelPanelItems = computed<ChannelPanelItem[]>(() => {
   const chList: ChannelPanelItem[] = [];
   const activeChannel = route.params.id;
-  if (device.value) {
-    for (const [k, v] of Object.entries(device.value.channels) as [
-      k: string,
-      v: Protobuf.Channel.Channel,
-    ][]) {
-      if (v.role !== Protobuf.Channel.Channel_Role.DISABLED) {
-        chList.push({
-          id: v.index,
-          label: v.settings?.name || 'Unknown',
-          to: `/chat/broadcast/${v.index}`,
-          active: activeChannel === v.index.toString(),
-        });
-      }
-    }
+
+  if (!device.value) return chList;
+
+  for (const [, v] of Object.entries(device.value.channels) as [
+    string,
+    Protobuf.Channel.Channel,
+  ][]) {
+    if (v.role === Protobuf.Channel.Channel_Role.DISABLED) continue;
+
+    const unreadCount = device.value.getUnreadCount(v.index);
+
+    chList.push({
+      id: v.index,
+      label: v.settings?.name || 'Unknown',
+      to: `/chat/broadcast/${v.index}`,
+      active: activeChannel === v.index.toString(),
+      badge: unreadCount > 0 ? unreadCount : undefined,
+      severity: unreadCount > 0 ? 'info' : undefined,
+    });
   }
+
   return chList;
 });
 </script>
