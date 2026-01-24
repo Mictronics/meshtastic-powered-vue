14ad59cf95b855c5c2c3744c0b93e7dc8f989f01
Mictronics <github@mictronics.de>
2026-01-24 22:28:54 +0100

    Commit debugging version for CI test.

--- packages/web/src/composables/core/subscriptions.ts
+++ packages/web/src/composables/core/subscriptions.ts
@@ -1,11 +1,8 @@
 import PacketToMessageDTO from "@/composables/core/dto/PacketToMessageDTO.ts";
-import { useNewNodeNum } from "@/composables/core/hooks/useNewNodeNum";
-import {
-  type IDevice,
-  type IMessageStore,
-  MessageType,
-  type INodeDB,
-} from "@/composables/core/stores";
+import { useNewNodeNum } from "@/composables/core/utils/useNewNodeNum";
+import { type IDevice } from '@/composables/core/stores/device/useDeviceStore'
+import { type IMessageStore, MessageType } from '@/composables/core/stores/message/useMessageStore'
+import { type INodeDB } from '@/composables/core/stores/nodeDB/useNodeDBStore'
 import { type MeshDevice, Protobuf } from "@meshtastic/core";
 export const subscribeAll = (
   device: IDevice,
@@ -16,10 +13,11 @@ export const subscribeAll = (
   let myNodeNum = 0;
 
   connection.events.onDeviceMetadataPacket.subscribe((metadataPacket) => {
-    device.addMetadata(metadataPacket.from, metadataPacket.data);
+    device.addMetadata(metadataPacket.data);
   });
 
   connection.events.onRoutingPacket.subscribe((routingPacket) => {
+    console.log("onRoutingPacket");
     switch (routingPacket.data.variant.case) {
       case "errorReason": {
         if (
@@ -41,8 +39,8 @@ export const subscribeAll = (
     }
   });
 
-  connection.events.onTelemetryPacket.subscribe(() => {
-    // device.setMetrics(telemetryPacket);
+  connection.events.onTelemetryPacket.subscribe((telemetryPacket) => {
+    nodeDB.setMetrics(telemetryPacket);
   });
 
   connection.events.onDeviceStatus.subscribe((status) => {
@@ -50,11 +48,13 @@ export const subscribeAll = (
   });
 
   connection.events.onWaypointPacket.subscribe((waypoint) => {
+    console.log("onWaypointPacket");
     const { data, channel, from, rxTime } = waypoint;
     device.addWaypoint(data, channel, from, rxTime);
   });
 
   connection.events.onMyNodeInfo.subscribe((nodeInfo) => {
+    console.log("onMyNodeInfo");
     useNewNodeNum(device.id, nodeInfo);
     myNodeNum = nodeInfo.myNodeNum;
   });
@@ -64,6 +64,7 @@ export const subscribeAll = (
   });
 
   connection.events.onPositionPacket.subscribe((position) => {
+    console.log("onPositionPacket");
     nodeDB.addPosition(position);
   });
 
@@ -75,16 +76,20 @@ export const subscribeAll = (
   });
 
   connection.events.onChannelPacket.subscribe((channel) => {
+    console.log("onChannelPacket");
     device.addChannel(channel);
   });
   connection.events.onConfigPacket.subscribe((config) => {
+    console.log("onConfigPacket");
     device.setConfig(config);
   });
   connection.events.onModuleConfigPacket.subscribe((moduleConfig) => {
+    console.log("onModuleConfigPacket");
     device.setModuleConfig(moduleConfig);
   });
 
   connection.events.onMessagePacket.subscribe((messagePacket) => {
+    console.log("onMessagePacket");
     // incoming and outgoing messages are handled by this event listener
     const dto = new PacketToMessageDTO(messagePacket, myNodeNum);
     const message = dto.toMessage();
@@ -102,16 +107,19 @@ export const subscribeAll = (
   });
 
   connection.events.onTraceRoutePacket.subscribe((traceRoutePacket) => {
+    console.log("onTraceroutePacket");
     device.addTraceRoute({
       ...traceRoutePacket,
     });
   });
 
   connection.events.onPendingSettingsChange.subscribe((state) => {
+    console.log("onPendingSettingsChange");
     device.setPendingSettingsChanges(state);
   });
 
   connection.events.onMeshPacket.subscribe((meshPacket) => {
+    console.log("onMeshPacket");
     nodeDB.processPacket({
       from: meshPacket.from,
       snr: meshPacket.rxSnr,
@@ -121,16 +129,19 @@ export const subscribeAll = (
 
   connection.events.onClientNotificationPacket.subscribe(
     (clientNotificationPacket) => {
+      console.log("onClientNotificationPacket");
       device.addClientNotification(clientNotificationPacket);
       device.setDialogOpen("clientNotification", true);
     },
   );
 
   connection.events.onNeighborInfoPacket.subscribe((neighborInfo) => {
+    console.log("onNeighborInfoPacket");
     device.addNeighborInfo(neighborInfo.from, neighborInfo.data);
   });
 
   connection.events.onRoutingPacket.subscribe((routingPacket) => {
+    console.log("onRoutingPacket");
     if (routingPacket.data.variant.case === "errorReason") {
       switch (routingPacket.data.variant.value) {
         case Protobuf.Mesh.Routing_Error.MAX_RETRANSMIT:
