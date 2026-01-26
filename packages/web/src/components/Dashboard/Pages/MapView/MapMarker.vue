f1186419a8ae39a61cd87b36ac77753a22cd843a
Mictronics <github@mictronics.de>
2026-01-26 20:54:49 +0100

    Added more node information to popover.

--- packages/web/src/components/Dashboard/Pages/MapView/MapMarker.vue
+++ packages/web/src/components/Dashboard/Pages/MapView/MapMarker.vue
@@ -16,21 +16,73 @@
       <Popover
         ref="popup"
         class="rounded-xl! border-slate-200! dark:border-slate-600! bg-white dark:bg-slate-800"
+        pt:content:style="padding: 0.3rem !important"
       >
-        <h3 class="font-bold">{{ node.longName || 'Node ' + node.nodeNumber }}</h3>
+        <h3 class="font-medium text-slate-800 dark:text-slate-400 mb-1 truncate">
+          {{ node.longName }}
+        </h3>
         <hr class="my-1" />
-        <p class="text-sm text-slate-700 dark:text-slate-400">Last position {{ timeAgo }}</p>
+        <div class="flex justify-between">
+          <NodeFeatures :node="node" />
+          <MessageSquare :size="15" />
+        </div>
+        <div class="space-y-1">
+          <div class="flex justify-between text-xs text-slate-500">
+            <span>
+              {{ node.shortName }}
+            </span>
+            <span>
+              {{ hexNodeNumber }}
+            </span>
+          </div>
+          <div class="flex justify-between gap-3 text-xs text-slate-500">
+            <span>
+              {{ node.hwModel }}
+            </span>
+            <span>
+              {{ node.snr }}
+            </span>
+          </div>
+          <div class="flex justify-between gap-3 text-xs text-slate-500">
+            <span>{{ node.hopsAway }} {{ lastHeard }}</span>
+            <BatteryStatus
+              v-if="!!node.deviceMetrics"
+              :batteryLevel="node.deviceMetrics.batteryLevel"
+            />
+          </div>
+        </div>
+        <hr class="my-1" />
+        <div class="space-y-1">
+          <div class="flex justify-around text-xs text-slate-800 dark:text-slate-400">
+            <MountainSnow v-if="!!node.position?.altitude" />
+            <span v-if="!!node.deviceMetrics?.channelUtilization">Channel Util.</span>
+            <span v-if="!!node.deviceMetrics?.airUtilTx">Air TX</span>
+          </div>
+          <div class="flex justify-around text-xs text-slate-500">
+            <span v-if="!!node.position?.altitude">{{ `${node.position?.altitude} m` }}</span>
+            <span v-if="!!node.deviceMetrics?.channelUtilization">
+              {{ node.deviceMetrics?.channelUtilization }}
+            </span>
+            <span v-if="!!node.deviceMetrics?.airUtilTx">
+              {{ node.deviceMetrics?.airUtilTx }}
+            </span>
+          </div>
+        </div>
       </Popover>
     </template>
   </mgl-marker>
 </template>
 
 <script setup lang="ts">
+import { MountainSnow, MessageSquare } from 'lucide-vue-next';
 import { ref, computed } from 'vue';
 import { MglMarker } from '@indoorequal/vue-maplibre-gl';
 import NodeAvatar from '@/components/Dashboard/NodeAvatar.vue';
+import NodeFeatures from '@/components/Dashboard/Pages/NodeView/NodeFeatures.vue';
+import BatteryStatus from '@/components/Dashboard/BatteryStatus.vue';
 import type { FormattedNode } from '@/composables/core/utils/types';
-import { useTimeAgoIntl } from '@vueuse/core';
+import { formatTimeAgoIntl } from '@vueuse/core';
+import { numberToHexUnpadded } from '@noble/curves/utils.js';
 
 const props = defineProps<{
   node: FormattedNode;
@@ -44,13 +96,12 @@ const emit = defineEmits<{
 
 const lat = computed(() => props.node.position?.latitudeI ?? 0);
 const lng = computed(() => props.node.position?.longitudeI ?? 0);
-const lastSeenMs = computed(() => {
-  const p = props.node.position;
-  if (!p) return 0;
-
-  return p.timestamp + p.timestampMillisAdjust;
+const lastHeard = computed(() => {
+  const date = new Date(0);
+  date.setUTCSeconds(props.node.lastHeard);
+  return formatTimeAgoIntl(date);
 });
-const timeAgo = useTimeAgoIntl(lastSeenMs);
+const hexNodeNumber = computed(() => '!' + numberToHexUnpadded(props.node.nodeNumber));
 const scale = computed(() => Math.min(1, Math.max(0.6, props.zoom / 9)));
 
 const popup = ref<any>();
@@ -59,3 +110,9 @@ const togglePopover = (event: MouseEvent) => {
   popup.value?.toggle(event);
 };
 </script>
+
+<style lang="css" scoped>
+.p-popover-content {
+  padding: 0.3rem !important;
+}
+</style>
