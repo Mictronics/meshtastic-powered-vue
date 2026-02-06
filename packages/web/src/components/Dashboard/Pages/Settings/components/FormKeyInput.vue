49174462af52e07a7760ee0605eccdd5d3acb0b1
Mictronics <github@mictronics.de>
2026-02-06 17:11:10 +0100

    Created form key input.

---
+++ packages/web/src/components/Dashboard/Pages/Settings/components/FormKeyInput.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="contents">
+    <InputGroup>
+      <InputText
+        size="small"
+        :feedback="false"
+        :invalid="!!error"
+        :disabled="disabled"
+        v-model="base64Key"
+      />
+      <InputGroupAddon>
+        <Button
+          :disabled="!isSupported || !base64Key"
+          size="small"
+          variant="text"
+          severity="secondary"
+          @click="copyKey"
+        >
+          <CopyCheck v-if="copied" :size="15" />
+          <Copy v-else :size="15" />
+        </Button>
+        <span class="pe-1 text-sm">256 Bit</span>
+      </InputGroupAddon>
+    </InputGroup>
+    <p v-if="error" class="text-red-400 dark:text-red-600 text-xs">{{ error }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useClipboard } from '@vueuse/core';
+import { Copy, CopyCheck } from 'lucide-vue-next';
+
+const base64Key = defineModel<string>('base64Key');
+
+const props = withDefaults(
+  defineProps<{
+    error?: string | boolean;
+    disabled?: boolean;
+  }>(),
+  {
+    error: false,
+    disabled: false,
+  }
+);
+
+const { copy, copied, isSupported } = useClipboard();
+const copyKey = async () => {
+  try {
+    await copy(base64Key.value ?? '');
+  } catch {}
+};
+</script>
