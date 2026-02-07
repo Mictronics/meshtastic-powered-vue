52881c453c6c1752b06253d3b76af8164a1360cf
Mictronics <github@mictronics.de>
2026-02-07 12:00:36 +0100

    Increased width of middle column.

--- packages/web/src/components/Dashboard/Pages/Settings/components/FormGrid.vue
+++ packages/web/src/components/Dashboard/Pages/Settings/components/FormGrid.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-1 sm:grid-cols-[0.2fr_0.2fr_1fr] items-center gap-x-3 gap-y-2 pt-2">
+  <div class="grid grid-cols-1 sm:grid-cols-[0.2fr_0.4fr_0.8fr] items-center gap-x-2 gap-y-2 pt-2">
     <slot />
   </div>
 </template>
