84a504ae76b0a47d54a5bd610cbbf5a0f50231e8
Mictronics <github@mictronics.de>
2026-02-20 20:12:32 +0100

    Added template files for module settings.

---
+++ packages/web/src/components/Dashboard/Pages/Settings/subforms/RangeTestModule.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="pt-2">
+    <div>
+      <h4 class="scroll-m-20 text-xl tracking-tight font-medium">Range Test Module</h4>
+      <p class="text-slate-400">Settings for the range test</p>
+    </div>
+    <FormGrid></FormGrid>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Validation } from '@vuelidate/core';
+import FormGrid from '../components/FormGrid.vue';
+import FormRow from '../components/FormRow.vue';
+
+defineProps<{
+  v$: Validation;
+}>();
+</script>
---
+++ packages/web/src/components/Dashboard/Pages/Settings/subforms/SerialModule.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="pt-2">
+    <div>
+      <h4 class="scroll-m-20 text-xl tracking-tight font-medium">Serial Module</h4>
+      <p class="text-slate-400">Settings for serial communication</p>
+    </div>
+    <FormGrid></FormGrid>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Validation } from '@vuelidate/core';
+import FormGrid from '../components/FormGrid.vue';
+import FormRow from '../components/FormRow.vue';
+
+defineProps<{
+  v$: Validation;
+}>();
+</script>
